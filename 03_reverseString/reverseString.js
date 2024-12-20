fd3ae36210119e9a7ba70e196f888ef550d906d7
Vivek <137711336+vivekkumar2202@users.noreply.github.com>
2024-12-20 06:12:32 +0000

    done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let reverseStr = "";
+    for (let i = str.length - 1; i >= 0; i--){
+        reverseStr += str[i];
+    }
+    return reverseStr;
 };
+
+reverseString("hello")
 
 // Do not edit below this line
 module.exports = reverseString;
