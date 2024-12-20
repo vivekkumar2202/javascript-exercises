501c0984cb9262871cda96094d3db1b4b7073336
Vivek <137711336+vivekkumar2202@users.noreply.github.com>
2024-12-20 07:52:22 +0000

    done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(arr, toRemove1 = null, toRemove2 = null, toRemove3 = null, toRemove4 = null) {
+    let newArray = arr.filter( el => el !== toRemove1 && el !== toRemove2 && el !== toRemove3 && el !== toRemove4);
+    return newArray;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
