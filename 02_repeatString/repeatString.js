c28fb7198bd9a87a8a798e46e0896f570c03fe94
Vivek <137711336+vivekkumar2202@users.noreply.github.com>
2024-12-20 06:01:53 +0000

    done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(toRepeat, no_of_times) {
+    let str = "";
+    if (no_of_times < 0){
+        return "ERROR";
+    }
+    for (let i=0; i<no_of_times; i++){
+        str += toRepeat;
+    }
+    return str;
 };
 
 // Do not edit below this line
