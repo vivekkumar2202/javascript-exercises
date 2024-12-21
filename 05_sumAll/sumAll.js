d4559a424993c2a4a5fad7aed3e5ba23d7e832b9
Vivek <137711336+vivekkumar2202@users.noreply.github.com>
2024-12-21 09:51:46 +0000

    done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if ((num1 < 0 || num2 < 0) 
+        || (Math.floor(num1) != num1 || Math.floor(num2) != num2)
+        || (typeof num1 !== 'number' || typeof num2 !== 'number')){
+        return 'ERROR';
+    }
+    let sum = 0;
+    if (num1 > num2){
+        let temp = num1;
+        num1 = num2;
+        num2 = temp;
+    } 
+    for (let i = num1; i <= num2; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
