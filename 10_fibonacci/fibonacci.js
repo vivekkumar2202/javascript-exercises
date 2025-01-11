fba899b07316067fe5239299ec2523decfc56771
Vivek <137711336+vivekkumar2202@users.noreply.github.com>
2025-01-11 08:27:14 +0000

    done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num = Number(num);
+    if (num < 0)
+        return "OOPS";
+    if (num === 0 )
+        return 0;
+    prev = 0;
+    curr = 1;
+    for (let i = 1; i < num; i++) {
+        let temp = prev;
+        prev = curr;
+        curr += temp;
+    }
+    return curr;
 };
 
 // Do not edit below this line
