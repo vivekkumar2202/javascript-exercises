3a53fe10cd4fec80616cf16cae329aff9a80de68
Vivek <137711336+vivekkumar2202@users.noreply.github.com>
2025-01-11 08:00:43 +0000

    done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,24 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    new_str = '';
+    for (let i = 0; i < str.length; i++) {
+        ascii = str[i].charCodeAt(0);
+        if (ascii > 64 && ascii < 91) {
+            new_str += str[i].toLowerCase();
+        }
+        else if ((ascii > 96 && ascii < 123) ||(ascii > 47 && ascii < 58)) {
+            new_str += str[i];
+        }
+    }
+    let i = 0;
+    let j = new_str.length - 1;
+    while (i < j) {
+        if (new_str[i] !== new_str[j]) {
+            return false;
+        }
+        i ++;
+        j --;
+    }
+    return true;
 };
 
 // Do not edit below this line
