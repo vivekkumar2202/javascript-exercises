343ec7ad115cdbb73bb60367f7035d7f674545b8
Vivek <137711336+vivekkumar2202@users.noreply.github.com>
2025-01-11 09:01:24 +0000

    done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,7 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.sort((a, b) => 
+        (((b.yearOfDeath || new Date().getFullYear()) - b.yearOfBirth) - 
+        ((a.yearOfDeath || new Date().getFullYear()) - a.yearOfBirth)))[0];
 };
 
 // Do not edit below this line
